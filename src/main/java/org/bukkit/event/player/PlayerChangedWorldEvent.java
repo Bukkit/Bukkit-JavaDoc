9558d201539eac0172f2bb706cc679df482d1971
iEpic <xcraaze@gmail.com>
2013-04-04 14:19:30 +0200

    Added description in JavaDoc
    + added return javadoc for the world player is coming from

--- src/main/java/org/bukkit/event/player/PlayerChangedWorldEvent.java
+++ src/main/java/org/bukkit/event/player/PlayerChangedWorldEvent.java
@@ -4,6 +4,9 @@ import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 
+/**
+ * Called when a player changes world
+ */ 
 public class PlayerChangedWorldEvent extends PlayerEvent {
     private static final HandlerList handlers = new HandlerList();
     private final World from;
@@ -12,7 +15,12 @@ public class PlayerChangedWorldEvent extends PlayerEvent {
         super(player);
         this.from = from;
     }
-
+    
+    /**
+     * Gets the world the player is teleporting from
+     * 
+     * @return worls player is teleporting from
+     */ 
     public World getFrom() {
         return from;
     }
