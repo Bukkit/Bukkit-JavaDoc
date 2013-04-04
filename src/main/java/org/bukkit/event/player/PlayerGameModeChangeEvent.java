4fd753fbd5506f4b27debf615e940f6a99f50c87
iEpic <xcraaze@gmail.com>
2013-04-04 13:55:54 +0200

    Added description in JavaDoc
    Added class description and return information

--- src/main/java/org/bukkit/event/player/PlayerGameModeChangeEvent.java
+++ src/main/java/org/bukkit/event/player/PlayerGameModeChangeEvent.java
@@ -5,6 +5,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
+/**
+ * Called when the GameMode of a Player is changed.
+ */
 public class PlayerGameModeChangeEvent extends PlayerEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private boolean cancelled;
@@ -23,6 +26,11 @@ public class PlayerGameModeChangeEvent extends PlayerEvent implements Cancellabl
         this.cancelled = cancel;
     }
 
+    /**
+     * Returns new GameMode of Player involved in the event
+     * 
+     * @return new GameMode of Player involved in the event
+     */
     public GameMode getNewGameMode() {
         return newGameMode;
     }
