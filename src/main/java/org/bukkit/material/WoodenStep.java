439e45a0337999aed904cf952b42c7f6b4fd8839
iEpic <xcraaze@gmail.com>
2013-04-05 12:04:24 +0200

    Fixed whitespaces

--- src/main/java/org/bukkit/material/WoodenStep.java
+++ src/main/java/org/bukkit/material/WoodenStep.java
@@ -55,7 +55,7 @@ public class WoodenStep extends MaterialData {
 
     /**
      * Test if step is inverted
-     * 
+     *
      * @return true if inverted (top half), false if normal (bottom half)
      */
     public boolean isInverted() {
@@ -64,7 +64,7 @@ public class WoodenStep extends MaterialData {
     
     /**
      * Set step inverted state
-     * 
+     *
      * @param inv - true if step is inverted (top half), false if step is normal (bottom half)
      */
     public void setInverted(boolean inv) {
