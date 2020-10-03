667b9ad79cfccbd31210812600df9713e4aeb6b5
harshit257 <55016133+harshit257@users.noreply.github.com>
2020-10-03 12:54:30 +0530

    Update package-info.java
    added a new function to compute simple interest

--- src/main/javadoc/org/bukkit/configuration/file/package-info.java
+++ src/main/javadoc/org/bukkit/configuration/file/package-info.java
@@ -3,6 +3,28 @@
  * org.bukkit.configuration.Configuration configurations} to be read and
  * stored on the filesystem.
  * <p>
- */
-package org.bukkit.configuration.file;
+ *
+
+import java.util.Scanner;
+
+public class scannerUser {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+//        int x = sc.nextInt();
+//        double y = sc.nextDouble();
+//        System.out.println(x);
+
+//        String hello= sc.nextLine();
+//        System.out.println(hello);
+
+        int principal =sc.nextInt();
+        float rate=sc.nextFloat();
+        int time=sc.nextInt();
+
+        float simpleInterest=principal*rate*time/100;
+        System.out.println("simple interst is "+simpleInterest);
+    }
+
+}
+
 
